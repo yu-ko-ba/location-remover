d606bb3a4be650ce7e28d332d68a0a86ff071018
k019c1395 <k019c1395@g.neec.axc.jp>
2021-12-13 14:01:02 +0900

    横幅指定

--- go.mod
+++ go.mod
@@ -7,4 +7,11 @@ require github.com/minodisk/go-fix-orientation v0.0.0-20160416014108-37c78c6093d
 require (
 	github.com/BurntSushi/graphics-go v0.0.0-20160129215708-b43f31a4a966 // indirect
 	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
+	github.com/shurcooL/go v0.0.0-20200502201357-93f07166e636 // indirect
+	github.com/shurcooL/go-goon v0.0.0-20210110234559-7585751d9a17 // indirect
+	github.com/shurcooL/goexec v0.0.0-20200425235707-36ff6d2d1adc // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- src/script.js
+++ src/script.js
@@ -1,16 +1,22 @@
 function previewFile(file) {
     // プレビュー画像を追加する要素
-    const preview = document.getElementById('preview');
+    // const preview = document.getElementById('preview');
     // FileReaderオブジェクトを作成
     const reader = new FileReader();
   
     // ファイルが読み込まれたときに実行する
     reader.onload = function (e) {
       const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
+
+      // const img = document.getElementById("preview");
+      // img.src = imageUrl;
+
       const img = document.createElement("img"); // img要素を作成
+      img.id = "previewImage"
       img.src = imageUrl; // 画像のURLをimg要素にセット
+      // img.width = 30;
       preview.appendChild(img); // #previewの中に追加
-      // console.log(imageUrl);
+      console.log(imageUrl);
     }
   
     // いざファイルを読み込む
